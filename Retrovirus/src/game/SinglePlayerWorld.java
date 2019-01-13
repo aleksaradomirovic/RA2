422e84b28a70edd9c6154c44f4b67f995fea28b3
Aleksa R <braleksa2@gmail.com>
2019-01-13 10:01:31 -0800

    restart

--- Retrovirus/src/game/SinglePlayerWorld.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public class SinglePlayerWorld extends World {
-	public SinglePlayerWorld() {
-		characters = new Player[1];
-	}
-}
--- Retrovirus/src/terrain/Road.java
+++
@@ -1,5 +0,0 @@
-package terrain;
-
-public class Road {
-	public static final int DIRT = 0, CITY = 1, HIGHWAY = 2;
-}
